9f9e39d90a0d6dacab0fb8a676b25fc68e91405a
felipepperez <fpproducoes@gmail.com>
2022-03-28 20:07:03 -0400

    Aula 28032022

---
+++ www/20220328/js/index.js
@@ -0,0 +1,15 @@
+var nome = "Felipe";
+var idade = 31;
+var emprego = "professor";
+
+console.log(nome,idade,emprego);
+
+console.log("Potencia",Math.pow(5,2)); //25
+
+console.log("Raiz",Math.sqrt(9)); //3
+
+console.log(2=="2"); //true
+
+console.log(2!="2"); //false
+
+console.log(2==="2"); //false
